d99d871e70674f2bd41637949b838279cb6c8000
P-005 Probe <test@repo-stitcher.local>
2026-09-05 01:42:30 +0530

    feat: add commitlint, husky, and lint-staged

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'revert',
+        'security',
+      ],
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+  },
+};
