6b5666fe497df4d0c306c0d2c4f81afe2c0c4bd1
P-005 Probe <test@repo-stitcher.local>
2026-09-05 18:18:22 +0530

    refactor(core): export OctokitAppAuth type from public API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -80,4 +80,9 @@ export {
 export type { Git, GitFactoryOptions } from './git/factory.js';
 
 export { createOctokit, request, getRepo, statusToStitchError } from './github/factory.js';
-export type { OctokitPatAuth, OctokitAuth, OctokitFactoryOptions } from './github/factory.js';
+export type {
+  OctokitPatAuth,
+  OctokitAppAuth,
+  OctokitAuth,
+  OctokitFactoryOptions,
+} from './github/factory.js';
